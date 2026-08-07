1466c06c66c809868d3ea0bf5adb70d6a07ac76d
thedon0012 <mohdsaadkhan0872@gmail.com>
2026-08-07 09:02:24 +0000

    Setup backend project structure

--- backend/src/main/java/com/nextcart/nextcart/NextcartBackendApplication.java
+++ backend/src/main/java/com/nextcart/nextcart/NextcartBackendApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NextcartBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NextcartBackendApplication.class, args);
+		System.out.println("Nextcart Backend Application started.");
+		//SpringApplication.run(NextcartBackendApplication.class, args);
 	}
 
 }
