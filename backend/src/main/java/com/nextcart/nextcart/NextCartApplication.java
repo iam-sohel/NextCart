444f48a06476c7d2df4d4efc55329ccd05ad3fd2
SaleemDMohaMMed <dsaleemdmd@gmail.com>
2026-08-17 18:40:55 +0530

    error done

---
+++ backend/src/main/java/com/nextcart/nextcart/NextCartApplication.java
@@ -0,0 +1,12 @@
+package com.nextcart.nextcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextCartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NextCartApplication.class, args);
+    }
+}
